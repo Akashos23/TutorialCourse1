22b6c6c6c83a0b3ee8ba0d84c7a43508f98af2f5
akash47 <65364908+Akashos23@users.noreply.github.com>
2025-10-15 09:41:05 +0200

    factoriel terminé

--- Factorial/src/factorial/Factorial.java
+++ Factorial/src/factorial/Factorial.java
@@ -2,4 +2,24 @@ package factorial;
 
 public class Factorial {
 
+	public int getFactorialRecursive(int n) {
+		if(n == 1 || n == 0) {
+			return n;
+		}
+		else {
+			return n * getFactorialRecursive(n - 1);
+		}
+	}
+	
+	public int getFactorialIterative(int n) {
+		if(n == 1 || n == 0) {
+			return n;
+		}
+		int res = 1;
+		for(int i = 2; i <= n; i++) {
+			res	= res * i;
+		}
+		return res;
+		
+	}
 }
